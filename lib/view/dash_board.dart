61ccfb83c38b4cc11ece363820fee890aa1b6a09
Arnoldaditya17 <adityasinghrajput@sistec.ac.in>
2024-08-31 17:36:28 +0530

    added navbar

---
+++ lib/view/dash_board.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:plant_health_monitor/view/app_drawer.dart';
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({super.key});
+
+  @override
+  State<DashBoard> createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF85C748),
+        title: Text("data"),
+        
+
+      ),
+        drawer: const AppDrawer(),
+      body: Text("data"),
+    );
+  }
+}
